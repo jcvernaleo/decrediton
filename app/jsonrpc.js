5c3dcb6f3ba8d74b91ef329b512d694d97c1564c
John C. Vernaleo <john@netpurgatory.com>
2017-06-30 12:59:54 -0400

    More

--- app/jsonrpc.js
+++ app/jsonrpc.js
@@ -1,29 +1,32 @@
 var WebSocket = require('ws');
 import { getCfg, getCert } from './config.js';
 
-var cfg = getCfg();
-var cert = getCert();
-var user = cfg.get('rpc_user');
-var password = cfg.get('rpc_pass');
-var url = 'wss://' + cfg.get('daemon_rpc_host') + ':' + cfg.RPCDaemonPort() + 'ws';
+//var cfg = getCfg();
+//var cert = getCert();
 
-var ws = new WebSocket(url, {
-  headers: {
-    'Authorization': 'Basic '+new Buffer(user+':'+password).toString('base64')
-  },
-  cert: cert,
-  ca: [cert]
-});
-ws.on('open', function() {
-  console.log('CONNECTED');
-  ws.send('{"jsonrpc":"1.0","id":"0","method":"getblockcount","params":[]}');
-});
-ws.on('message', function(data) {
-  console.log(data);
-});
-ws.on('error', function(err) {
-  console.log('ERROR:' + err);
-});
-ws.on('close', function() {
-  console.log('DISCONNECTED');
-});
+export function dcrdRPC(cfg, cert) {
+  var user = cfg.get('rpc_user');
+  var password = cfg.get('rpc_pass');
+  var url = 'wss://' + cfg.get('daemon_rpc_host') + ':' + cfg.RPCDaemonPort() + 'ws';
+
+  var ws = new WebSocket(url, {
+    headers: {
+      'Authorization': 'Basic '+new Buffer(user+':'+password).toString('base64')
+    },
+    cert: cert,
+    ca: [cert]
+  });
+  ws.on('open', function() {
+    console.log('CONNECTED');
+    ws.send('{"jsonrpc":"1.0","id":"0","method":"getblockcount","params":[]}');
+  });
+  ws.on('message', function(data) {
+    console.log(data);
+  });
+  ws.on('error', function(err) {
+    console.log('ERROR:' + err);
+  });
+  ws.on('close', function() {
+    console.log('DISCONNECTED');
+  });
+}
