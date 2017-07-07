09613d335b4f9ed99cbdf38e2ab0cccd24b84a0d
John C. Vernaleo <john@netpurgatory.com>
2017-07-07 14:52:58 -0400

    id and some generalizing

--- app/jsonrpc.js
+++ app/jsonrpc.js
@@ -13,30 +13,40 @@ export function dcrdRPC(cfg, cert, method) {
     cert: cert,
     ca: [cert]
   });
+  var id = new Date().getTime();
   ws.on('open', function() {
+    var cmdString = '';
+    var cmdStart = '{"jsonrpc":"1.0","id":"';
     console.log('CONNECTED');
     switch(method) {
     case 'getblockcount':
-      ws.send('{"jsonrpc":"1.0","id":"0","method":"getblockcount","params":[]}');
+      cmdString = cmdStart + id.toString() + '","method":"getblockcount","params":[]}';
       break;
     case 'ping':
-      ws.send('{"jsonrpc":"1.0","id":"0","method":"ping","params":[]}');
+      cmdString = cmdStart + id.toString() + '","method":"ping","params":[]}';
       break;
     case 'getbestblock':
-      ws.send('{"jsonrpc":"1.0","id":"0","method":"getbestblock","params":[]}');
+      cmdString = cmdStart + id.toString() + '","method":"getblestblock","params":[]}';
       break;
     case 'stop':
-      ws.send('{"jsonrpc":"1.0","id":"0","method":"stop","params":[]}');
+      cmdString = cmdStart + id.toString() + '","method":"stop","params":[]}';
       break;
     default:
       console.log('Unsupported method: ', method);
     }
+    if (cmdString !== '') {
+      //console.log(cmdString);
+      ws.send(cmdString);
+    }
   });
   ws.on('message', function(data) {
     var res = JSON.parse(data)
-    console.log(res.result);
-    ws.close();
-    return res.result
+    if (res.id == id) {
+      //console.log(res)
+      console.log(res.result);
+      ws.close();
+      return res.result;
+    }
   });
   ws.on('error', function(err) {
     console.log('ERROR:' + err);
